091bf57624e00b17f4ead2492af80bebb9f289ef
mowebiaz <mowebiaz@gmail.com>
2024-03-12 16:21:43 +0100

    Fix bug on Menu
    Add id to sections on home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -16,7 +16,7 @@ const Page = () => {
 	/* const {last} = useData() */
 	const { data } = useData()
 	const sortedEvents = data?.events.sort((evtA, evtB) => new Date(evtB.date) - new Date(evtA.date))
-	const last = sortedEvents?.[0] 
+	const last = sortedEvents?.[0]
 	return (
 		<>
 			<header>
@@ -26,7 +26,7 @@ const Page = () => {
 				<section className='SliderContainer'>
 					<Slider />
 				</section>
-				<section className='ServicesContainer'>
+				<section className='ServicesContainer' id='nos-services'>
 					<h2 className='Title'>Nos services</h2>
 					<p>Nous organisons des événements sur mesure partout dans le monde</p>
 					<div className='ListContainer'>
@@ -50,11 +50,11 @@ const Page = () => {
 						</ServiceCard>
 					</div>
 				</section>
-				<section className='EventsContainer'>
+				<section className='EventsContainer' id='nos-realisations'>
 					<h2 className='Title'>Nos réalisations</h2>
 					<EventList />
 				</section>
-				<section className='PeoplesContainer'>
+				<section className='PeoplesContainer' id='notre-equipe'>
 					<h2 className='Title'>Notre équipe</h2>
 					<p>Une équipe d’experts dédiés à l’ogranisation de vos événements</p>
 					<div className='ListContainer'>
@@ -93,7 +93,7 @@ const Page = () => {
 						}
 					>
 						{({ setIsOpened }) => <Form onSuccess={() => setIsOpened(true)} onError={() => null} />}
- </Modal>
+					</Modal>
 				</div>
 			</main>
 			<footer className='row'>
